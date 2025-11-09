a6e83ebfca52d20ffd128f2f3c8eaa06a785aa86
VyrakSna <snavyrak@gmail.com>
2025-11-09 17:13:02 +0700

    'change'

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from os import abort
-from tokenize import String
 import re
+from tokenize import String
 
-from click import Abort
-from flask import Flask, request
+from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text, false
 from flask_migrate import Migrate
@@ -25,6 +23,57 @@ def falsyChecker(param):
 def intChecker(param):
     if type(param) is not int:
         return {"message": "Bad request"}, 400
+
+def getallinvoicedetail():
+    invoice_details = InvoiceDetail.query.all()
+    result = []
+    for i in invoice_details:
+        orderitem = OrderItem.query.get(i.orderitem_id)
+        # assert False, orderitem
+        data = {
+            "id": i.id,
+            "orderitem_id": i.orderitem_id,
+            "orderitem": {
+                "id": orderitem.id,
+                "order_id": orderitem.order_id,
+                "product_id": orderitem.product_id,
+                "price": orderitem.price,
+                "quantity": orderitem.qty,
+                "total": orderitem.total
+            } if orderitem else None
+        }
+        result.append(data)
+    return result
+def getinvoicedetail(param):
+    i = InvoiceDetail.query.get(param)
+    orderitem = OrderItem.query.get(param)
+    data = {
+            "id": i.id,
+            "orderitem_id": i.orderitem_id,
+            "orderitem": {
+                "id": orderitem.id,
+                "order_id": orderitem.order_id,
+                "product_id": orderitem.product_id,
+                "price": orderitem.price,
+                "quantity": orderitem.qty,
+                "total": orderitem.total
+            } if orderitem else None
+        }
+    return data
+# def getorder(param):
+#     order = Order.query.get(param)
+#     if order is None:
+#         return None
+#
+#     data = {
+#         "Order_id": order.id,
+#         "user_id": order.user_id,
+#         "customer_id": order.customer_id,
+#         "date_time": order.date_time.isoformat() if order.date_time else None,
+#         "status": order.status
+#
+#     }
+#     return jsonify(data)
 # ========================= BEGIN USER API ==========================
 @app.route('/user/list')
 def show_user():
@@ -39,9 +88,9 @@ def show_user():
 def error_400(error):
     return 'Bad request', 400
 
-@app.errorhandler(500)
-def error_500(error):
-    return f'missing key: {error}', 500
+# @app.errorhandler(500)
+# def error_500(error):
+#     return f'missing key: {error}', 500
 
 @app.post('/user/create')
 def create_user():
@@ -100,14 +149,10 @@ def hello_world():
 def show_category():
     categories = Category.query.all()
     if not categories:
-        return {"message": "no user"}, 404
+        return {"message": "no category"}, 404
     # result = [{"id": c.id, "name": c.name} for c in categories]
     result = [{"id": c.id, "name": c.name} for c in categories]
     return {"Category": result}, 200
-
-@app.errorhandler(400)
-def error_400(error):
-    return 'Bad request', 400
 
 @app.post('/category/create')
 def create_category():
@@ -144,7 +189,7 @@ def delete_category(id):
 def get_product():
     product = Product.query.all()
     if not product:
-        return {"message": "no user"}, 404
+        return {"message": "no product"}, 404
     list_product = [{"name": p.name, "image": p.image, "cost": p.cost, "price": p.price, "category_id": p.category_id, "stock": p.stock, "description": p.description} for p in product]
     return {"product": list_product}, 200
 @app.post('/product/create')
@@ -158,7 +203,7 @@ def create_product():
     price = req['price']
     stock = req['stock']
     description = req.get('description')
-    if image and image.filename is not '':
+    if image and image.filename != '':
         file_name = secure_filename(image.filename)
         image.save(f'./image/product/{file_name}')
     else:
@@ -181,7 +226,7 @@ def update_product(id):
     price = req.get('price') or int(product.price)
     stock = req.get('stock') or int(product.stock)
     description = req.get('description') or product.description
-    if image and image.filename is not '':
+    if image and image.filename != '':
         file_name = secure_filename(image.filename)
         image.save(f'./image/product/{file_name}')
     else:
@@ -204,7 +249,9 @@ def delete_product(id):
 @app.route('/invoice/list')
 def get_invoice():
     invoice = Invoice.query.all()
-    result = [{"id": i.id, "invoice_detail_id": i.invoice_detail_id, "order_id": i.order_id} for i in invoice]
+    if not invoice:
+        return {"message": "no invoice"}, 404
+    result = [{"id": i.id, "invoice_detail_id": getinvoicedetail(i.invoice_detail_id), "order_id": i.order_id} for i in invoice]
     return {"invoice": result}, 200
 @app.post('/invoice/create')
 def create_invoice():
@@ -218,7 +265,6 @@ def create_invoice():
 
 @app.put('/invoice/<int:id>/update')
 def update_invoice(id):
-    invoice = Invoice.query.get(id)
     req = request.json
     order_id = req.get("order_id")
     invoice_detail_id = req.get("invoice_detail_id")
@@ -234,12 +280,10 @@ def delete_invoice(id):
     db.session.commit()
     return {"message": "resource is delete"}, 200
 # ========================= END INVOICE API ======================
-# ========================= BEGIN INVOICEDETAIL API ======================
+# ========================= END INVOICEDETAIL API ======================
 @app.route('/invoicedetail/list')
 def get_invoicedetail():
-    invoice_detail = InvoiceDetail.query.all()
-    result = [{"id": i.id, "orderitem_id": i.orderitem_id} for i in invoice_detail]
-    return {"invoice_detail": result}, 200
+    return {"invoice_detail": getallinvoicedetail()}, 200
 @app.post('/invoicedetail/create')
 def create_invoicedetail():
     req = request.json
@@ -268,6 +312,8 @@ def delete_invoicedetail(id):
     db.session.commit()
     return {"message": "resource is delete"}, 200
 # ========================= END INVOICEDETAIL API ======================
+# ========================= BEGIN SALE REPORT API ======================
 
+# ========================= END SALE REPORT API ======================
 if __name__ == '__main__':
     app.run()
